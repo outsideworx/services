3896afd3e3c7d981a673adfb6ede5735aeaad7e6
hornokattila <hornokattila@outlook.com>
2025-11-05 17:14:48 +0100

    Removed enableWebSecu explicit.

--- src/main/java/application/configuration/WebSecurity.java
+++ src/main/java/application/configuration/WebSecurity.java
@@ -3,12 +3,10 @@ package application.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 class WebSecurity {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
