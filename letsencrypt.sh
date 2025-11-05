6749f1bd5b8f02a0878d2d49f288d6f1ed520c29
hornokattila <hornokattila@outlook.com>
2025-11-05 09:52:28 +0100

    Refactor.

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$1" == "--remote" ]; then
-    if [ -z "$2" ]; then
-        echo "Error: IP address is required as the second parameter!"
-        exit 1
-    fi
+if [ -z "$2" ]; then
+    echo "Error: IP address is required as the second parameter!"
+    exit 1
+fi
+SERVER_IP="$2"
 
-    SERVER_IP="$2"
+if [ "$1" == "--remote" ]; then
     # WARNING: For this section to work, port 80 has to be open and accessible via the below mentioned address.
     ssh root@"$SERVER_IP" "
         apt update;
