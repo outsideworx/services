a42068913814434ead1dd46fe2ef5e96bf5f4f57
hornokattila <hornokattila@outlook.com>
2025-12-04 17:38:49 +0100

    Added postgres-exporter

--- src/main/java/application/SpringApplication.java
+++ src/main/java/application/SpringApplication.java
@@ -3,7 +3,7 @@ package application;
 @org.springframework.cache.annotation.EnableCaching
 @org.springframework.boot.autoconfigure.SpringBootApplication
 class SpringApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
     }
 }
