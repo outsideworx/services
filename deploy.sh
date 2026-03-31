d17e08ed6349d676ab75b781b7f51a21edb1d110
Outside Worx <info@outsideworx.net>
2026-03-31 13:24:26 +0000

    Transfer to local deployment.

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+DEST="/home/outsideworx/services"
+
+if [ "$1" == "--letsencrypt" ]; then
+    # WARNING: For this section to work, port 80 has to be open and accessible via the below mentioned address.
+    apt update
+    apt install -y certbot
+    certbot certonly --standalone --noninteractive --agree-tos --email info@outsideworx.net -d services.outsideworx.net
+fi
+
+echo "Packaging project"
+mvn clean package -f "$SCRIPT_DIR/pom.xml"
+
+echo "Copying project files to: $DEST"
+rm -rf "$DEST"
+mkdir -p "$DEST"
+cp "$SCRIPT_DIR/.env" \
+   "$SCRIPT_DIR/compose.yaml" \
+   "$SCRIPT_DIR/docker-stats.sh" \
+   "$SCRIPT_DIR/docker-wipe.sh" \
+   "$SCRIPT_DIR/Dockerfile" \
+   "$SCRIPT_DIR/grafana.ini" \
+   "$SCRIPT_DIR/loki.yaml" \
+   "$SCRIPT_DIR/ntfy.yaml" \
+   "$SCRIPT_DIR/prometheus.yaml" \
+   "$DEST"
+cp -r "$SCRIPT_DIR/target" "$DEST"
+
+echo "Deployment starts"
+cd "$DEST"
+docker compose build --no-cache --pull
+docker compose up --force-recreate --no-deps -d
+docker system prune -af
+docker logs services -f
---
+++ docker-wipe.sh
@@ -0,0 +1,13 @@
+read -p "Start? (y/n) " ans
+[[ $ans =~ ^[Yy]$ ]] || { echo "Aborted."; return; }
+
+docker stop $(docker ps -qa)
+docker rmi -f $(docker images -qa)
+docker system prune -af
+
+rm -rf /home/outsideworx/services
+rm -rf /home/outsideworx/sites
+
+apt update -y
+apt upgrade -y
+htop
